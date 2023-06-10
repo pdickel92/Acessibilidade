121635a2f95a24274c19ebc1c7c447df3f7435a3
Peterson Dickel <petercristiano.dickel@gmail.com>
2023-06-10 14:13:08 -0300

    adicionado local

---
+++ src/telas/Home/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default ({ navigation }) => {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('CameraScreen')}
+        >
+          <Text style={styles.buttonText}> Tela Camera </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, { marginVertical: 10 }]}
+          onPress={() => navigation.navigate('InstrucaoScreen')}
+        >
+          <Text style={styles.buttonText}> Instrução </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      button: {
+        backgroundColor: '#5f9ea0',
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 4,
+      },
+      buttonText: {
+        color: '#fff',
+        fontSize: 16,
+      }
+})
